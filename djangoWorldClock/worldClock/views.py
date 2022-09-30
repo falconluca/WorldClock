bd1f66fd3d688fc0714ef6d16312c7707aead179
Ayooluwa Isaiah <ayisaiah@gmail.com>
2022-09-30 14:21:53 +0100

    update repo to match article

--- djangoWorldClock/worldClock/views.py
+++ djangoWorldClock/worldClock/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render, redirect
 import requests
-import uuid
 
 # Create your views here.
 def home(request):
-    request.session["id"] = str(uuid.uuid4())
-
     return render(request, "home.html")
-
 
 def search(request):
 
@@ -18,26 +14,29 @@ def search(request):
     # Get the search query
     query = request.POST.get("q", "")
 
-    # Pass the search query to the Nominatim API to get a location
-    location = requests.get(
-        "https://nominatim.openstreetmap.org/search",
-        {"q": query, "format": "json", "limit": "1"},
-    ).json()
+    try:
+        # Pass the search query to the Nominatim API to get a location
+        location = requests.get(
+            "https://nominatim.openstreetmap.org/search",
+            {"q": query, "format": "json", "limit": "1"},
+        ).json()
 
-    # If a location is found, pass the coordinate to the Time API to get the current time
-    if location:
-        coordinate = [location[0]["lat"], location[0]["lon"]]
+        # If a location is found, pass the coordinate to the Time API to get the current time
+        if location:
+            coordinate = [location[0]["lat"], location[0]["lon"]]
 
-        time = requests.get(
-            "https://timeapi.io/api/Time/current/coordinate",
-            {"latitude": coordinate[0], "longitude": coordinate[1]},
-        )
+            time = requests.get(
+                "https://timeapi.io/api/Time/current/coordinate",
+                {"latitude": coordinate[0], "longitude": coordinate[1]},
+            )
 
-        return render(
-            request, "success.html", {"location": location[0], "time": time.json()}
-        )
+            return render(
+                request, "success.html", {"location": location[0], "time": time.json()}
+            )
 
-    # If a location is NOT found, return the error page
-    else:
+        # If a location is NOT found, return the error page
+        else:
 
-        return render(request, "fail.html")
+            return render(request, "fail.html")
+    except Exception as error:
+        return render(request, "500.html")
